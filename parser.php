ffdccb2ea6e9152de8e8ecf0f08deeac1cc6750d
Sam Ottenhoff <ottenhoff@longsight.com>
2022-05-02 21:52:53 -0400

    2022 updates

--- parser.php
+++ parser.php
@@ -2,11 +2,11 @@
 include 'vendor/autoload.php';
 
 $parser = new \Smalot\PdfParser\Parser();
-$years = array('2016', '2017', '2018', '2019', '2020', '2021');
+$years = array('2016', '2017', '2018', '2019', '2020', '2021', '2022');
 $entries = array();
 
 // Recent Sales info
-$sales_files = array('single.pdf', 'multi.pdf');
+$sales_files = array('single.pdf', 'multi.pdf', 'commercial.pdf', 'vacant.pdf', 'condo.pdf');
 foreach ($sales_files AS $file) {  
   $pdf    = $parser->parseFile($file);
   $pages  = $pdf->getPages();
@@ -15,8 +15,11 @@ foreach ($sales_files AS $file) {
     $lines = explode("\n", $blob);
 
     foreach (array_reverse($lines) AS $line) {
+
       if (strlen ($line) > 70) {
-        $tmp = preg_split('/\s+/', $line);
+        $cleaned_line = str_replace("$", "", $line);
+        $cleaned_line = str_replace(",", "", $cleaned_line);
+        $tmp = preg_split('/\s+/', $cleaned_line);
         $id = trim($tmp[0]);
 
         if ($file == 'multi.pdf') {
@@ -172,14 +175,16 @@ ksort($entries);
 
 $fp = fopen('output.csv', 'w');
 fputcsv($fp, array('ID', 'Street', 'Non-Homestead', 'Account', 'Code', '2016 City', '2016 Full', '2017 City', '2017 Full', '2018 City', '2018 Full', 
-  '2019 City', '2019 Full', '2020 City', '2020 Full', '2021 City', '2021 Full', 'Sale Date', 'Sale Price', 'Sale AV', 
-  '2016OwnerAtAddress', '2017OwnerAtAddress', '2018OwnerAtAddress', '2019OwnerAtAddress', '2020OwnerAtAddress', '2021OwnerAtAddress',
-  'Latitude', 'Longitude', 'TigerLine', '20162020Diff', 'SaleAssessDiff',
+  '2019 City', '2019 Full', '2020 City', '2020 Full', '2021 City', '2021 Full', '2022 City', '2022 Full',
+  'Sale Date', 'Sale Price', 'Sale AV', 
+  '2016OwnerAtAddress', '2017OwnerAtAddress', '2018OwnerAtAddress', '2019OwnerAtAddress', '2020OwnerAtAddress', '2021OwnerAtAddress', '2022OwnerAtAddress',
+  'Latitude', 'Longitude', 'TigerLine', 'DistanceFromCityHall',
+  '20162020Diff', 'SaleAssessDiff',
   'SqFt', 'Bathrooms', 'Bedrooms', 'YearBuilt', 'LandSize', 'Zoning', 'Ward', 'CensusBlock',
   'AVPerSqFt', 'AVChangePercent', 'SalePricePerSqFt', 'NbhdCode', '2021Vacant', '2021Violations', 'OpportunityZone',
 ));
 foreach ($entries AS $id => $data) {
-  if (empty($data['2020fullmarket'])) continue;
+  if (empty($data['2022fullmarket'])) continue;
   #if (!isset($data['latitude'])) continue;
   #if (!isset($data['sqft'])) continue;
 
@@ -201,6 +206,8 @@ foreach ($entries AS $id => $data) {
     $data['2020fullmarket'],
     $data['2021taxvalue'],
     $data['2021fullmarket'],
+    $data['2022taxvalue'],
+    $data['2022fullmarket'],
     $data['saledate'],
     $data['saleprice'],
     $data['saleassess'],
@@ -210,10 +217,12 @@ foreach ($entries AS $id => $data) {
     isset($data['2019owneraddress']) ? $data['2019owneraddress'] : 0, 
     isset($data['2020owneraddress']) ? $data['2020owneraddress'] : 0, 
     isset($data['2021owneraddress']) ? $data['2021owneraddress'] : 0, 
+    isset($data['2022owneraddress']) ? $data['2022owneraddress'] : 0, 
     $data['latitude'],
     $data['longitude'],
     $data['tiger'],
-    (isset($data['2016fullmarket']) && isset($data['2021fullmarket'])) ? (int) $data['2021fullmarket'] - (int) $data['2016fullmarket'] : '',
+    (!empty($data['latitude']) && !empty($data['longitude'])) ? greatCircleDistance($data['latitude'], $data['longitude']) : 0,
+    (isset($data['2016fullmarket']) && isset($data['2022fullmarket'])) ? (int) $data['2022fullmarket'] - (int) $data['2016fullmarket'] : '',
     (isset($data['saleprice']) && isset($data['saleassess'])) ? (int) $data['saleprice'] - (int) $data['saleassess'] : '',
     $data['sqft'],
     $data['bathrooms'],
@@ -223,8 +232,8 @@ foreach ($entries AS $id => $data) {
     $data['zoning'],
     $data['ward'],
     $data['census_block'],
-    (!empty($data['sqft']) && !empty($data['2021fullmarket'])) ? (int) $data['2021fullmarket'] / (int) $data['sqft'] : '',
-    (!empty($data['2016fullmarket']) && !empty($data['2021fullmarket'])) ? ((int) $data['2021fullmarket'] - (int) $data['2016fullmarket']) / (int) $data['2016fullmarket'] : '',
+    (!empty($data['sqft']) && !empty($data['2022fullmarket'])) ? (int) $data['2022fullmarket'] / (int) $data['sqft'] : '',
+    (!empty($data['2016fullmarket']) && !empty($data['2022fullmarket'])) ? ((int) $data['2022fullmarket'] - (int) $data['2016fullmarket']) / (int) $data['2016fullmarket'] : '',
     (!empty($data['saleprice'])) ? $data['saleprice'] / $data['sqft'] : '',
     $data['nbhd'],
     (empty($data['2020vacant']) ? 0 : 1),
@@ -251,4 +260,19 @@ var_dump($average_of_foo);die();
 function clean($str) {
   $tmp = trim($str);
   return preg_replace("/[^0-9.]/", "", $tmp);
+}
+
+function greatCircleDistance( $latitudeFrom, $longitudeFrom, $latitudeTo=41.499992, $longitudeTo=-74.010009, $earthRadius = 6371000) {
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $latDelta = $latTo - $latFrom;
+  $lonDelta = $lonTo - $lonFrom;
+
+  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+    cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+  return $angle * $earthRadius;
 }
